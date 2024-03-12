dfdc0816faee3717b3e42401d677f11647d7804b
Pema Rinchen <111570488+Pema-Rinchen@users.noreply.github.com>
2024-03-12 14:06:45 +0530

    Create Vehicle .java

---
+++ Assignment 3/Vehicle .java
@@ -0,0 +1,65 @@
+public class Vehicle {
+    // Class variables
+    private static int totalVehicles = 0;
+
+    // Instance variables
+    private String make;
+    private String model;
+    private int year;
+    private double price;
+
+    // Constructor
+    public Vehicle(String make, String model, int year, double price) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.price = price;
+        totalVehicles++;
+    }
+
+    // Getter and Setter methods
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    // Static method
+    public static int getTotalVehicles() {
+        return totalVehicles;
+    }
+
+    // Other methods
+    public void displayDetails() {
+        System.out.println("Make: " + make);
+        System.out.println("Model: " + model);
+        System.out.println("Year: " + year);
+        System.out.println("Price: $" + price);
+    }
+}
