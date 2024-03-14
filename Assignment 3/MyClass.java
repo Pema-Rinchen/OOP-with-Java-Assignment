6d071fd7201287a896904b75e86fecd313031336
Pema Rinchen <111570488+Pema-Rinchen@users.noreply.github.com>
2024-03-14 19:32:25 +0530

    Create MyClass.java

---
+++ Assignment 3/MyClass.java
@@ -0,0 +1,12 @@
+public class MyClass {
+
+    // Default constructor
+    public MyClass() {
+        System.out.println("Default constructor called.");
+    }
+
+    public static void main(String[] args) {
+        // Creating an instance of MyClass
+        MyClass obj = new MyClass();
+    }
+}
