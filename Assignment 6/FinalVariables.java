2a4dff8dba7f296692a8d3ab6a0ca62af5793bd6
Pema Rinchen <111570488+Pema-Rinchen@users.noreply.github.com>
2024-03-20 20:09:43 +0530

    Create FinalVariables.java

---
+++ Assignment 6/FinalVariables.java
@@ -0,0 +1,9 @@
+public class FinalVariables {
+    public static void main(String[] args) {
+        final int x = 5;
+        final int y = 10;
+        // Uncommenting the following will cause a compile-time error
+        // x = 10;
+        // y = 5;
+    }
+}
